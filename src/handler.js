daf5d2b9d37a4dda6f9952be662415417b3f005e
najwaafc <136673744+najwaafc@users.noreply.github.com>
2023-12-14 20:27:41 +0700

    Update handler.js

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,38 @@
 const communities = require('./community');
 const { nanoid } = require('nanoid');
+const { Storage } = require('@google-cloud/storage');
 
 // menambahkan komunitas
+// const addCommunity = (request, h) => {
+//   const {
+//     name,
+//     location,
+//     description,
+//     members,
+//     status,
+//     type
+//   } = request.payload;
+
+//   // Validasi data yang diterima
+//   if (!name || !location || !description || members === undefined) {
+//     return h.response({ status: 'fail', message: 'Gagal menambahkan komunitas. Mohon isi data dengan lengkap' }).code(400);
+//   }
+
+//   const id = nanoid();
+
+//   const newCommunity = {
+//     id,
+//     name,
+//     location,
+//     description,
+//     members,
+//     status,
+//     type
+//   };
+
+//   communities.push(newCommunity);
+//   return h.response({ status: 'success', message: 'Komunitas berhasil ditambahkan', data: { communityId: id } }).code(201);
+// };
 const addCommunity = (request, h) => {
   const {
     name,
@@ -9,7 +40,8 @@ const addCommunity = (request, h) => {
     description,
     members,
     status,
-    type
+    type,
+    image
   } = request.payload;
 
   // Validasi data yang diterima
@@ -17,8 +49,34 @@ const addCommunity = (request, h) => {
     return h.response({ status: 'fail', message: 'Gagal menambahkan komunitas. Mohon isi data dengan lengkap' }).code(400);
   }
 
-  const id = nanoid();
+  // Validasi file gambar
+  if (!image || !image.file || typeof image.file !== 'object') {
+    return h.response({ status: 'fail', message: 'Gagal menambahkan komunitas. Mohon upload gambar komunitas' }).code(400);
+  }
 
+  const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg'];
+  if (!allowedMimeTypes.includes(image.file.mimetype)) {
+    return h.response({ status: 'fail', message: 'Format gambar tidak valid' }).code(400);
+  }
+  if (image.file.size > 2097152) {
+    return h.response({ status: 'fail', message: 'Ukuran gambar terlalu besar' }).code(400);
+  }
+
+
+  // Upload gambar ke Cloud Storage
+  const cloudStorageClient = new Storage();
+
+  const imageFileName = `${id}.${image.file.ext}`;
+
+  // Upload gambar ke Cloud Storage (removed await)
+  cloudStorageClient.upload(
+    `communities/${imageFileName}`,
+    image.file.data
+  );
+
+  const imageUrl = `https://${edupath-bucket}.storage.googleapis.com/${imageFileName}`;
+
+  // Simpan URL gambar ke komunitas baru
   const newCommunity = {
     id,
     name,
@@ -26,11 +84,20 @@ const addCommunity = (request, h) => {
     description,
     members,
     status,
-    type
+    type,
+    imageUrl
   };
 
   communities.push(newCommunity);
-  return h.response({ status: 'success', message: 'Komunitas berhasil ditambahkan', data: { communityId: id } }).code(201);
+  // Tambahkan URL gambar ke response
+  return h.response({
+    status: 'success',
+    message: 'Komunitas berhasil ditambahkan',
+    data: {
+      communityId: id,
+      imageUrl
+    }
+  }).code(201);
 };
 
 //getAll
@@ -44,7 +111,8 @@ const getAllCommunityHandler = () => {
       location: community.location,
       members: community.members,
       status: community.status,
-      type: community.type
+      type: community.type,
+      image:community.imageUrl
       })),
     }
   };
