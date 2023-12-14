d7ab91c7b4e5c238fc83e48f1d3b2867646d6a85
najwaafc <136673744+najwaafc@users.noreply.github.com>
2023-12-14 13:56:00 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,1 @@
-const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host :process.env.DB_HOST ,
-    user : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD,
-    database : 'edupath-db',
-});
-
-module.exports=connection;
